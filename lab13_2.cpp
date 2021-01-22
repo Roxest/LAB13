f4aadfdbdb427603b4cfeb86fb73d91ea71570b6
Roxest <thammarin82@gmail.com>
2021-01-22 19:03:07 +0700

    FINISHED

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -19,3 +19,44 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void inputMatrix(double inpMat[N][N]){
+for (int i = 0; i < N; i++){
+	cout<<"Row "<<i+1<<": ";
+	for (int j = 0; j < N; j++)
+	{
+		cin>>inpMat[i][j];
+	}
+}
+}
+
+void findLocalMax(const double A[][N], bool B[][N]){
+for (int i = 0; i < N; i++){
+	for (int j = 0; j < N; j++)
+	{
+		B[i][j]=0;
+	}
+}
+for (int i = 1; i < N-1; i++){
+	for (int j = 1; j < N-1; j++){
+		if(A[i][j]>=A[i-1][j] && A[i][j]>=A[i+1][j] && A[i][j]>=A[i][j-1] &&A [i][j]>=A[i][j+1])
+		{
+			B[i][j]=1;
+		}
+		else
+		{
+			B[i][j]=0;
+		}
+		
+	}
+}
+}
+
+void showMatrix (const bool inf[][N]){
+for (int i = 0; i < N; i++){
+	for (int j = 0; j < N; j++){
+		cout<<inf[i][j]<<" ";
+	}
+	cout<<"\n";
+}
+}
